378e206014f69c29afaee99b0c4ab18013e1dd12
Dhruv Narang <narang.dhruv123@gmail.com>
2024-09-08 23:36:38 +0530

    First Episode

---
+++ App.js
@@ -0,0 +1,19 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "grandchild" }, "I'm an H1 tag"),
+    React.createElement("h2", { id: "heading" }, "I'm an H2 tag"),
+  ])
+);
+
+console.log(parent);
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello world from react!!"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
