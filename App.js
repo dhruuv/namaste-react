9092fbcc1029f1c31159d1ab36139e4ca633d524
Dhruv Narang <narang.dhruv123@gmail.com>
2024-09-09 01:32:57 +0530

    Second Episode

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
+import React from "react";
+import ReactDOM from "react-dom"
+
+const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", { id: "grandchild" }, "I'm an H1 tag"),
     React.createElement("h2", { id: "heading" }, "I'm an H2 tag"),
-  ])
-);
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "grandchild" }, "I'm an H1 tag"),
+    React.createElement("h2", { id: "heading" }, "I'm an H2 tag"),
+  ]),
+]);
 
 console.log(parent);
 
